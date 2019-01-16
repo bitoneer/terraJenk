2e05ab051ced97d83ca26ac08c9f85e0f68ef429
Tim Mason <timm@bitoneer.com>
2019-01-16 12:22:22 +0000

    Getting rid of the sudo stuff

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # install java
-sudo apt update
-sudo apt install -y openjdk-8-jdk
+apt update
+apt install -y openjdk-8-jdk
 
 # install jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install -y jenkins=2.150.1
+apt-get update
+apt-get install -y jenkins=2.150.1
 
 # wait for jenkins up
 while ! nc -z localhost 8080 ; do sleep 1 ; done
 
 # bypass the startup wizard 
-sudo cat > /var/lib/jenkins/jenkins.install.UpgradeWizard.state << EOF
+cat > /var/lib/jenkins/jenkins.install.UpgradeWizard.state << EOF
 2.0
 EOF
 
-sudo cat > /var/lib/jenkins/init.groovy.d/basic-security.groovy << EOF
+cat > /var/lib/jenkins/init.groovy.d/basic-security.groovy << EOF
 #!groovy
 
 import jenkins.model.*
@@ -41,14 +41,14 @@ EOF
 # set the cli port
 sudo apt install xmlstarlet
 xmlstarlet -q ed -u "//slaveAgentPort" -v "49187" /var/lib/jenkins/config.xml > /tmp/jenkins_config.xml
-sudo mv /tmp/jenkins_config.xml /var/lib/jenkins/config.xml
-sudo service jenkins restart
+mv /tmp/jenkins_config.xml /var/lib/jenkins/config.xml
+service jenkins restart
 
 # wait for jenkins up
 while ! nc -z localhost 8080 ; do sleep 1 ; done
 
 # get the admin password
-sudo cp /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar /var/lib/jenkins/jenkins-cli.jar
+cp /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar /var/lib/jenkins/jenkins-cli.jar
 PASS=$(sudo bash -c "cat /var/lib/jenkins/secrets/initialAdminPassword")
 
 
