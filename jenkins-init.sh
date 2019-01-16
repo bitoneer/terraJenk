9249c49bd3ffd1055ebb8b4302a5bc5a69e22f1d
Tim Mason <timm@bitoneer.com>
2019-01-16 13:14:16 +0000

    Trying to fix multiline cat issue

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -x
 
 # install java
 apt update
@@ -18,15 +19,13 @@ cat > /var/lib/jenkins/jenkins.install.UpgradeWizard.state << EOF
 2.0
 EOF
 
-cat > /var/lib/jenkins/init.groovy.d/basic-security.groovy << EOF
+sudo sh -c "cat > /var/lib/jenkins/init.groovy.d/basic-security.groovy <<EOF
 #!groovy
 
 import jenkins.model.*
 import hudson.security.*
 
 def instance = Jenkins.getInstance()
-
-println "--> creating local user 'admin'"
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount('{{ jenkins_admin_username }}','{{ jenkins_admin_password }}')
@@ -37,12 +36,13 @@ strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
 instance.save()
 EOF
+"
 
 # set the cli port
 sudo apt install xmlstarlet
-xmlstarlet -q ed -u "//slaveAgentPort" -v "49187" /var/lib/jenkins/config.xml > /tmp/jenkins_config.xml
-mv /tmp/jenkins_config.xml /var/lib/jenkins/config.xml
-service jenkins restart
+sudo xmlstarlet -q ed -u "//slaveAgentPort" -v "49187" /var/lib/jenkins/config.xml > /tmp/jenkins_config.xml
+sudo mv /tmp/jenkins_config.xml /var/lib/jenkins/config.xml
+sudo service jenkins restart
 
 # wait for jenkins up
 while ! nc -z localhost 8080 ; do sleep 1 ; done
